f554db4f3a88ecee79f30672b987cc006a197335
Grantlyk <grant_leadbetter@live.co.uk>
2015-01-12 17:34:06 +0000

    Removed unnecessary lines of code

--- expired_products.rb
+++ expired_products.rb
@@ -7,10 +7,10 @@ require 'zip'
 require 'open-uri'
 require 'json'
 
+
 content = open("http://www.shopshopgo.com/get_data_feeds.json").read
 data_feed_download_url = Hash.new {|hash, key| hash[key] = []}
 data_feed_download_url = JSON.parse(content)
-
 products = []
 expired_products = []
 data1 = {}
@@ -30,14 +30,6 @@ while d < data_feed_download_url.length do
   `curl -o ~/SearchTheSales/scripts/expired_products/#{products[d]}2.csv.zip #{data_feed_download_url[d]["feed_url"]}`
   d +=1
 end
-
-
-# `curl -o ~/SearchTheSales/scripts/expired_products/asos2.csv.zip http://datafeed.api.productserve.com/datafeed/download/apikey/5ceb2936022c9df973e19221f267ec28/cid/129,595,539,147,149,135,163,168,159,169,161,167,170,171,174,183,178,179,175,172,189,194,141,205,198,206,203,208,199,204,201/fid/5678/columns/aw_product_id,merchant_product_id,merchant_category,aw_deep_link,merchant_image_url,search_price,description,product_name,rrp_price,display_price,brand_name,size,specifications,merchant_deep_link/format/csv/delimiter/,/compression/zip/adultcontent/0/`
-# `curl -o ~/SearchTheSales/scripts/expired_products/hof2.csv.zip http://datafeed.api.productserve.com/datafeed/download/apikey/5ceb2936022c9df973e19221f267ec28/cid/171,174,183,178,179,175,172,189,194,198,206,199,204,201/fid/3100/columns/aw_product_id,merchant_product_id,merchant_category,aw_deep_link,merchant_image_url,search_price,description,product_name,rrp_price,display_price,brand_name,specifications,size,colour,stock_quantity,merchant_deep_link/format/csv/delimiter/,/compression/zip/adultcontent/0/`
-# `curl -o ~/SearchTheSales/scripts/expired_products/flannels2.csv.zip http://datafeed.api.productserve.com/datafeed/download/apikey/5ceb2936022c9df973e19221f267ec28/fid/3805/columns/aw_product_id,merchant_product_id,merchant_category,aw_deep_link,merchant_image_url,search_price,description,product_name,rrp_price,display_price,brand_name,specifications,merchant_deep_link/format/csv/delimiter/,/compression/zip/adultcontent/0/`
-
-
-conn = PG::Connection.open(:dbname => 'test')
 
 
 
